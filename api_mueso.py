9ba59875119f57b9994c63edf27f1aa116bb451e
marcelocarpio <m.carpio@correo.unimet.edu.ve>
2025-08-05 15:39:52 -0400

    Funciones para cargar la API del Museo

---
+++ api_mueso.py
@@ -0,0 +1,103 @@
+import requests
+from Departamento import Departamento
+from Obra_Arte import ObraDeArte
+from Artista import Artista
+
+def db_inicial():
+    resp= requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+    return resp.json()
+
+def cargar_departamentos():
+    """
+    Obtiene la lista de departamentos del museo y los convierte en objetos.
+    Retorna una lista de objetos Departamento.
+    """
+    
+    intentos=3
+    while intentos > 0:
+        resp= requests.get("https://collectionapi.metmuseum.org/public/collection/v1/departments")
+
+        if resp.status_code == 200:
+            datos_json = resp.json()
+            departamentos_list = []
+            dic_datos=datos_json.get("departments")
+
+            for dato in dic_datos:
+                departamentos_list.append(Departamento(dato["departmentId"],dato["displayName"], []))
+
+            print("Departamentos obtenidos con éxito.")
+            return departamentos_list
+            
+        else:
+            print(f"Intento fallido. Código de estado: {resp.status_code}. Intentos restantes: {intentos - 1}")
+            intentos -= 1
+    
+    print("No se pudo obtener la lista de departamentos.")
+    return []
+
+def cargar_ids_por_departamento(id_departamento):
+    """
+    IDs de las obras para un departamento específico.
+    Retorna una lista de IDs de las obras.
+    
+    Args:
+        departamento_id (int): El ID del departamento.
+    """
+
+    intentos = 3
+    while intentos > 0:
+        resp = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds={id_departamento}")
+
+        if resp.status_code == 200:
+            ids_json = resp.json()
+            lista_ids_obras=(ids_json.get("objectIDs"))
+            print(f"Se encontraron {len(lista_ids_obras)} obras para el departamento {id_departamento}.")
+            return lista_ids_obras
+        
+        else:
+            print(f"Error. Código de estado: {resp.status_code}. Intentos restantes: {intentos - 1}")
+            intentos -= 1
+            
+    print(f"No se pudo obtener la lista de IDs.")
+    return []
+
+def cargar_detalles_obra(id_obra):
+    """
+    Obtiene los detalles de una obra y crea un objeto Artista y un objeto ObraDeArte.
+    
+    Args:
+        id_obra (int): El ID de la obra de arte.
+    """
+    intentos = 3
+    while intentos > 0:
+        resp = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{id_obra}")
+        
+        if resp.status_code == 200:
+            id_obra_json = resp.json()
+            
+            artista = Artista(nombre=id_obra_json.get("artistDisplayName"), nacionalidad=id_obra_json.get("artistNationality"), nacimiento=id_obra_json.get("artistBeginDate"), muerte=id_obra_json.get("artistEndDate"))
+
+            obra = ObraDeArte(id=id_obra_json.get("objectID"), titulo=id_obra_json.get("title"), artista=artista, clasificacion=id_obra_json.get("classification"), anio=id_obra_json.get("objectDate"), url_imagen=id_obra_json.get("primaryImage"))
+
+            print(f"Detalles de la obra {id_obra} obtenidos con éxito.")
+            return obra
+        else:
+            print(f"Error. Código de estado: {resp.status_code}. Intentos restantes: {intentos - 1}")
+            intentos -= 1
+            
+    print(f"No se pudieron obtener los detalles de la obra {id_obra}.")
+    return None
+
+def cargar_nacionalidades_lista():
+    """
+    Crea una lista de nacionalidades desde el archivo CSV.
+    Retorna una lista con las nacionalidades.
+    """
+    nacionalidades = []
+    with open('lista_nacionalidades.csv', 'r') as lista_nacionalidades:
+        
+        for nacion in lista_nacionalidades:
+            pais=nacion.strip()
+            nacionalidades.append(pais)
+    print("Lista de nacionalidades obtenida con éxito.")
+    return nacionalidades
