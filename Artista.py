a73da4170478eb195c199062c916a9defc8ef250
marcelocarpio <m.carpio@correo.unimet.edu.ve>
2025-08-05 10:03:49 -0400

    Delete Artista.py

--- Artista.py
+++
@@ -1,6 +0,0 @@
-class Artista:
-    def __init__(self, nombre, nacionalidad, nacimiento, muerte):
-        self.nombre=nombre
-        self.nacionalidad=nacionalidad
-        self.nacimiento=nacimiento
-        self.muerte=muerte
