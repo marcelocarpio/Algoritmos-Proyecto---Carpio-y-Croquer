47728162e32f34fcd28aa8a109ef3ba1baa534bd
marcelocarpio <m.carpio@correo.unimet.edu.ve>
2025-08-05 10:01:31 -0400

    Add files via upload

---
+++ Obra_Arte.py
@@ -0,0 +1,9 @@
+class ObraDeArte:
+    def __init__(self, id, titulo, artista, clasificacion, anio, url_imagen):
+        self.id=id
+        self.titulo=titulo
+        self.artista=artista
+        self.clasificacion=clasificacion
+        self.anio=anio
+        self.url_imagen=url_imagen
+
