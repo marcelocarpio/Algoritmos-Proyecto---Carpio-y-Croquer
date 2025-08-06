cea6e677568480d4849cbd0ab50a456a5d5dcf6c
Andres A. Croquer C. <andres.croquer@correo.unimet.edu.ve>
2025-08-06 15:37:32 -0400

    Update Obra_Arte.py
    Se añadió el método resumen y mostrar_detalles

--- Obra_Arte.py
+++ Obra_Arte.py
@@ -7,3 +7,19 @@ class ObraDeArte:
         self.anio=anio
         self.url_imagen=url_imagen
 
+    def resumen(self):
+        print(f"{self.id}: {self.titulo} - {self.artista.nombre}")
+
+    def mostrar_detalles(self):
+        print(f"ID: {self.id}")
+        print(f"Título: {self.titulo}")
+        print(f"Artista: {self.artista.nombre}")
+        print(f"Nacionalidad: {self.artista.nacionalidad}")
+        print(f"Fecha de Nacimiento: {self.artista.nacimiento}")
+        print(f"Fecha de Muerte: {self.artista.muerte}")
+        print(f"Clasificación: {self.clasificacion}")
+        print(f"Año de creación: {self.anio}")
+        if self.url_imagen:
+            print(f"URL de la imagen: {self.url_imagen}")
+        else:
+            print("No hay imagen disponible.")
